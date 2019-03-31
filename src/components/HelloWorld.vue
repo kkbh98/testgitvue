04a0ec3de34539ada5d608f34577a27889dcfe6e
kkbh98 <duckduck0054@naver.com>
2019-03-31 17:27:50 +0900

    UUUUU

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
       AAAAA
       BBBBB
       CCCCC
+      UUUUU
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
