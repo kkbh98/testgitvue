1eaaf670ef63320c820efba04db88c926001fa16
kkbh98 <duckduck0054@naver.com>
2019-03-31 15:17:05 +0900

    AAAAA

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       check out the 
       DEVELOP의 COMMIT 하나를 FEATURE에 CHERRY-PICK으로 가져와서 COMMIT AND PUSH TEST
       git checkout --orphan name_you_choose_for_orphan_branch
+      AAAAA
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
